15c13bc94a5dae626210cdb1e8998e182b1eb2e0
2005unni <aneeshunni22r@gmail.com>
2022-09-07 17:15:32 +0530

    update

---
+++ assets/scripts/config.js
@@ -0,0 +1,23 @@
+Config = {}; // Don't touch
+
+// Social media buttons on the left side
+Config.Socials = [
+    {name: "discord", icon: "assets/media/icons/discord.png", link: "https://https://discord.gg/TmV2MfVTTx.gg/"},
+    {name: "instagram", icon: "assets/media/icons/instagram.png", link: "https://www.instagram.com/crazy_notics"},
+    {name: "youtube", icon: "assets/media/icons/youtube.png", link: "https://www.youtube.com/channel/UCTtrSHcI326WV9KWHW-i2zA"},
+];
+
+Config.HideoverlayKeybind = 112 // JS key code https://keycode.info
+Config.CustomBindText = "F1"; // leave as "" if you don't want the bind text in html to be statically set
+
+// Team list
+Config.Team = [
+    {name: "ANEESH", description: "DEV", color: "#fff", image: "https://raw.githubusercontent.com/2005unni/about/main/making%20about_files/IMG_4480-modified-scaled-assets/IMG_4480-modified-scaled.png"},
+    {name: "VIBIN", description: "DEV", color: "#fff", image: "https://raw.githubusercontent.com/2005unni/about/main/making%20about_files/nvin.png"},
+    {name: "DINOY", description: "STAFF", color: "#fff", image: "https://raw.githubusercontent.com/2005unni/about/main/making%20about_files/image_6483441.jpg"},
+    {name: "ADARSH", description: "STAFF", color: "#fff", image: "https://raw.githubusercontent.com/2005unni/about/main/making%20about_files/image_6483441-1.jpg"},
+    {name: "ASWATHY", description: "STAFF", color: "#fff", image: "https://raw.githubusercontent.com/2005unni/about/main/making%20about_files/PRAJEESH-KP-3.jpg"},
+    {name: "SANUSHA", description: "STAFF", color: "#fff", image: "https://raw.githubusercontent.com/2005unni/about/main/making%20about_files/HARI.png"},
+    {name: "FARHANA", description: "STAFF", color: "#fff", image: "https://raw.githubusercontent.com/2005unni/about/main/making%20about_files/ADARSH.png"},
+    {name: "APARNA", description: "STAFF", color: "#fff", image: "https://raw.githubusercontent.com/2005unni/about/main/making%20about_files/PRAVEEN-RV.png"},
+];
